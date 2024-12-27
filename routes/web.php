6bd1af1cf79c375521bf50dd5fb383503e9d5fbb
Hana Kamila Naura Yasmin <135443822+hanakamilanaura@users.noreply.github.com>
2024-12-27 15:54:49 +0700

    update views

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/vacation/index', [VacationController::class, 'index'])->name('vacation.index');
+    Route::get('/vacation', [VacationController::class, 'index'])->name('vacation.index');
 });
 
 require __DIR__.'/auth.php';
