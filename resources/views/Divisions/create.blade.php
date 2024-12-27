83927f8b6bbb6ba7d842bfa27419ab037f1609f6
cheze9 <130385704+cheze9@users.noreply.github.com>
2024-12-28 02:16:02 +0700

    Menambahkan file untuk fitur division

---
+++ resources/views/Divisions/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Divisi</h1>
+    <form action="{{ route('employee.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="name">Nama</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="no_telp">No Telepon</label>
+            <input type="text" class="form-control" id="no_telp" name="no_telp" required>
+        </div>
+        <div class="form-group">
+            <label for="address">Alamat</label>
+            <textarea class="form-control" id="address" name="address" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+        <a href="{{ route('employee.index') }}" class="btn btn-secondary">Kembali</a>
+    </form>
+</div>
+@endsection
